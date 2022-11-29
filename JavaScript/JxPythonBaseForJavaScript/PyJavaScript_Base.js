7f2f2f201cf67597c0ff1f680d06dbaa391c6063
Jacob-xyb <949782197@qq.com>
2022-11-29 15:59:25 +0800

    采用原型方式改写

--- JavaScript/JxPythonBaseForJavaScript/PyJavaScript_Base.js
+++ JavaScript/JxPythonBaseForJavaScript/PyJavaScript_Base.js
@@ -1,9 +1,19 @@
-class PyString {
-    constructor(py_str = "") {
-        this.data = py_str
-    }
+String.prototype.py_startswith = function (prefix, start = 0, end = this.length) {
+    return this.slice(start, end).startsWith(prefix);
+}
 
-    startswith(prefix) {
-        return Boolean(this.data.indexOf(prefix) === 0);
+String.prototype.py_endswith = function (suffix, start = 0, end = this.length) {
+    return this.slice(start, end).endsWith(suffix);
+}
+
+String.prototype.py_replace = function (old_s, new_s, count = Infinity) {
+    if (count === Infinity) {
+        return this.replaceAll(old_s, new_s);
+    } else {
+        let res = this.slice();
+        while (count-- > 0 && res.includes(old_s)) {
+            res = res.replace(old_s, new_s);
+        }
+        return res;
     }
 }
